1b0caf6df8aed5e48a807cbf79bab5d0e9fe37f6
Bett Kipkemoi <121342739+bettkipkemoi@users.noreply.github.com>
2023-03-21 01:46:48 -0400

    update

--- sankeyapp.py
+++ sankeyapp.py
@@ -8,4 +8,8 @@ st.title('Sankey Diagram (Alluvial Plots in R)')
 
 # loading dataset
 migration = pd.read_csv('migration_nz.csv')
+
+st.subheader('Exploring the dataset')
 st.dataframe(migration)
+st.caption('Dataset Details')
+st.text_area('New Zealand Population Migration dataset has information about the number of people who departed from and arrived in New Zealand from all continents and countries of the world from 1979 to 2016.')
