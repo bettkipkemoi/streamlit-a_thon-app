86f42a2abe1a9d1dce371df5f957a7dc432fdb0c
Bett Kipkemoi <121342739+bettkipkemoi@users.noreply.github.com>
2023-03-21 02:35:41 -0400

    update

--- sankeyapp.py
+++ sankeyapp.py
@@ -16,19 +16,4 @@ st.caption('Dataset Details')
 st.text_area('New Zealand Population Migration dataset has information about the number of people who departed from and arrived in New Zealand from all continents and countries of the world from 1979 to 2016.')
 
 # Generating Sankey Diagram
-fig = go.Figure(data=[go.Sankey(
-    node = dict(
-      pad = 15,
-      thickness = 20,
-      line = dict(color = "black", width = 0.5),
-      label = ["A1", "A2", "B1", "B2", "C1", "C2"],
-      color = "blue"
-    ),
-    link = dict(
-      source = [0, 1, 0, 2, 3, 3], # indices correspond to labels, eg A1, A2, A1, B1, ...
-      target = [2, 3, 3, 4, 4, 5],
-      value = [8, 4, 2, 8, 4, 2]
-  ))])
-
-fig.update_layout(title_text="Basic Sankey Diagram", font_size=10)
-fig.show()
+st.plotly_chart(figure_or_data= migration)
